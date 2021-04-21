d7782e10d8c810f1ee688e5b5a9c39a42cdff5bf
Dazhi Wang <ch2ohch2oh@gmail.com>
2021-04-21 15:48:42 -0400

    Add 14 - longest common prefix

---
+++ 14.java
@@ -0,0 +1,24 @@
+// Java string 
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0) {
+            return "";
+        } else if(strs.length == 1) {
+            return strs[0];
+        } else {
+            int longest = strs[0].length();
+            for(int i=1; i<strs.length; i++) {
+                String a = strs[i-1];
+                String b = strs[i];
+                int pos = 0;
+                while(pos<a.length() && pos < b.length() && a.charAt(pos) == b.charAt(pos)) {
+                    pos++;
+                }
+                if(pos < longest) {
+                    longest = pos;
+                }
+            }
+            return strs[0].substring(0, longest);
+        }
+    }
+}
