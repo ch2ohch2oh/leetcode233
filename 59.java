45a564ef05c4749eb680c1d2fddd527977a31bf2
Dazhi Wang <ch2ohch2oh@gmail.com>
2021-04-21 15:12:58 -0400

    Add 59 - spiral matrix ii

---
+++ 59.java
@@ -0,0 +1,23 @@
+// Fill a matrix in spiral order
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int [][] res = new int[n][n];
+        int dir = 0;
+        int [][] moves = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int row = 0;
+        int col = 0;
+        for(int i=1; i<=n*n; i++) {
+            res[row][col] = i;
+            int nextRow = row + moves[dir][0];
+            int nextCol = col + moves[dir][1];
+            if(nextRow < 0 || nextRow >= n || nextCol < 0 || nextCol >= n || res[nextRow][nextCol] > 0) {
+                dir = (dir + 1) % 4;
+                nextRow = row + moves[dir][0];
+                nextCol = col + moves[dir][1];
+            }
+            row = nextRow;
+            col = nextCol;
+        }
+        return res;
+    }
+}
