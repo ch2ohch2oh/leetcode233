1280665e43be939a0aa5c78a5ecce102abd93ab8
dazhi <ch2ohch2oh@gmail.com>
2021-04-21 02:08:37 -0400

    number of islands

---
+++ 200.java
@@ -0,0 +1,26 @@
+// DFS
+class Solution {
+    public int numIslands(char[][] grid) {
+        int num = 0;
+        for(int i=0; i < grid.length; i++) {
+            for(int j=0; j < grid[i].length; j++) {
+                if(grid[i][j] == '1') {
+                    clearIsland(grid, i, j);
+                    num += 1;
+                }
+            }
+        }
+        return num;
+    }
+    
+    private void clearIsland(char[][] grid, int i, int j) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        int [][] moves = {{0, 1}, {0, -1}, {-1, 0,}, {1, 0}};
+        for(int k=0; k < moves.length; k++) {
+            clearIsland(grid, i + moves[k][0], j + moves[k][1]);
+        }
+    }
+}
